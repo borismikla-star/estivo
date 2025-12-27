1a2fb16b35a78f7484f72a2a06f6fcc2db36445a
Boris Mikla <boris.mikla@gmail.com>
2025-12-27 21:40:45 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -491,7 +491,7 @@ export default function Layout({ children, currentPageName }) {
         </aside>
 
         {/* Main Content */}
-        <div className="flex-1 flex flex-col min-h-screen">
+        <div className="flex-1 flex flex-col">
           {/* Mobile Header */}
           <header className="lg:hidden flex items-center justify-between bg-card border-b border-border h-16 px-4 sticky top-0 z-50">
              <div className="flex items-center gap-2">
