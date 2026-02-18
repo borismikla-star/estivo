f3f4d04b6863148294863bc7b3ab83aaffad4abc
Boris Mikla <boris.mikla@gmail.com>
2026-02-18 09:45:09 +0000

    File changes

--- src/components/calculator/commercial/CapExInputs.jsx
+++ src/components/calculator/commercial/CapExInputs.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { Sparkles, Calculator } from "lucide-react";
 import InfoTooltip from "../../shared/InfoTooltip";
 
 export default function CapExInputs({ data, onChange, language = 'en' }) {
