4364ffa837ca8c70051ae8ce69a4a7e7e9ff4698
Boris Mikla <boris.mikla@gmail.com>
2026-02-19 09:50:11 +0000

    File changes

--- src/components/calculator/airbnb/PropertyInputs.jsx
+++ src/components/calculator/airbnb/PropertyInputs.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import InfoTooltip from '../../shared/InfoTooltip';
+import { useVatHints } from '../VatInputBanner';
 
 export default function PropertyInputs({ data, onChange, language = 'en' }) {
     const [localData, setLocalData] = useState(data);
