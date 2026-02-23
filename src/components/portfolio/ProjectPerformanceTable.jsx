44d599f3bdb38e9d427e64aef54536e2acbaf817
Boris Mikla <boris.mikla@gmail.com>
2026-02-23 11:50:52 +0000

    File changes

--- src/components/portfolio/ProjectPerformanceTable.jsx
+++ src/components/portfolio/ProjectPerformanceTable.jsx
@@ -317,7 +317,7 @@ export default function ProjectPerformanceTable({ projects, selectedIds, onSelec
                   <ArrowUpDown className="ml-1 h-3 w-3 lg:h-4 lg:w-4" />
                 </Button>
               </TableHead>
-              <SortableHeader t_key="roi" sort_key="roi" />
+              <SortableHeader t_key="roi" sort_key="roi" tooltip={t.roi_tooltip_rental} />
               <TableHead>{t.country}</TableHead>
             </TableRow>
           </TableHeader>
