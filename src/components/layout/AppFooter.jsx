b2d4c65bb87c9a656f9295ee3d15f5c44c0e534d
Boris Mikla <boris.mikla@gmail.com>
2026-03-03 12:30:57 +0000

    File changes

--- src/components/layout/AppFooter.jsx
+++ src/components/layout/AppFooter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
+import FeedbackButton from '@/components/shared/FeedbackButton';
 
 export default function AppFooter({ language = 'en' }) {
     const translations = {
